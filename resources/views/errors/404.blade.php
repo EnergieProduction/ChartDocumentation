5656c95cbbcfc086322d37aaa6198217c0c4eea3
DeGraciaMathieu <contact@degracia-mathieu.fr>
2017-09-02 20:39:11 +0200

    Adding error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.error')
+
+@section('doc-title', 'Error')
+@section('doc-logo', 'icon_datareport_alt')
+@section('doc-class', 'body-blue')
+
+@section('error')
+<h2 class="title">Erreur 404 (Page not found)</h2>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.error')
+
+@section('doc-title', 'Error')
+@section('doc-logo', 'icon_datareport_alt')
+@section('doc-class', 'body-blue')
+
+@section('error')
+<h2 class="title">Erreur 500 (Internal Server Error)</h2>
+@endsection
