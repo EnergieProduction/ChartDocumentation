5601b5ec75d3fa0e2bcdaaf8230390cbd2e27e4f
DeGraciaMathieu <contact@degracia-mathieu.fr>
2017-09-02 20:53:11 +0200

    Catch des erreurs fatales en production

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if (config('app.debug') && ! $this->isHttpException($exception)) {
+            $exception = new \Symfony\Component\HttpKernel\Exception\HttpException(500);
+        }
+
         return parent::render($request, $exception);
     }
 
